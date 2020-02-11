db9a547c755a017fa5679033e768d4b4fffb6eaa
JelleStiesri <jellestiesri@gmail.com>
2020-02-11 18:02:26 +0100

    11-2

--- Mastermind_hoofd.py
+++ Mastermind_hoofd.py
@@ -2,34 +2,39 @@ import random
 
 
 
-def mastermind():
+def menu():
     antwoord = []
     print("welke spelmodus wil je spelen",'\n',"1: Speler raadt, computer geeft code",'\n',"2: Computer raadt, speler geeft code")
     keuze = int(input('Keuze: '))
     if keuze == 1:
-        for getal in range(4):
-            los_getal = random.randrange(1, 7)
-            antwoord.append(los_getal)
+        sraad()
     elif keuze == 2:
-        while True: #True todat user goede code geeft
-            getallen = input("Geef een 4 cijferige code: ")
-            if len(getallen) != 4:
-                print('Probeer opnieuw')
-            else:
-                for getal in getallen:
-                    getal = int(getal)
-                    antwoord.append(getal)
-                break
+        craad()
 
 
 
 
+def craad():
+    antwoord = []
+    while True:  # True todat user goede code geeft
+        getallen = input("Geef een 4 cijferige code: ")
+        if len(getallen) != 4:
+            print('Probeer opnieuw')
+        else:
+            for getal in getallen:
+                getal = int(getal)
+                antwoord.append(getal)
+            break
+    print(antwoord)
 
-
-
+def sraad():
+    antwoord = []
+    for getal in range(4):
+        los_getal = random.randrange(1, 7)
+        antwoord.append(los_getal)
 
     print(antwoord)
 
-mastermind()
 
 
+menu()
