5375f6656c03ec593137833786c62dc4913d2d34
JelleStiesri <jellestiesri@gmail.com>
2020-02-13 14:42:48 +0100

    Feedback algoritme

--- Feedback.py
+++ Feedback.py
@@ -1,7 +1,37 @@
 def feedback(gok, antwoord):
+    wit = 0
+    zwart = 0
+    for item in gok: #voor de witte feedback
+        if item in antwoord:
+            wit += 1
+    index = 0
+    for item in gok: #voor de zwarte feedback
+        if item == antwoord[index]:
+            zwart += 1
+        index += 1
+    wit = wit-zwart
+    print('Zwart:',zwart, ' -- ','Wit:',wit)
+    return(zwart, wit)
 
 
 
+def gok():
+    gok = []
+    while True:  # True todat user goede code geeft
+        getallen = input("Gok: ")
+
+        if len(getallen) != 4:
+            print('Probeer opnieuw')
+        else:
+            for getal in getallen:
+                getal = int(getal)
+                gok.append(getal)
+            break
+    feedback(gok, [3, 7, 2, 3])
 
 
-def feedback(input)
+print(feedback([1, 7, 3, 2], [3, 7, 2, 3]))
+
+
+#while True:
+   # gok()
--- Test.py
+++ Test.py
@@ -1,11 +1,23 @@
-from itertools import permutations
+import random
 def gen():
-    perm = permutations([1, 2, 3, 4, 5, 6])
-    mogelijkheden = []
-    for i in list(perm):
-        mogelijkheden.append(i)
+    lijst = []
+    fout = 0
+    while True:
+        combinatie = []
+        for getal in range(4):
+            los_getal = random.randrange(1, 7)
+            combinatie.append(los_getal)
+        if combinatie in lijst:
+            fout = fout + 1
+            continue
+        else:
+            lijst.append(combinatie)
+        if len(lijst) == 1296: #aantal mogelijke combinaties
+            print('klaar')
+            break
+    lijst.sort(key=lambda x: x[0,1,2,3])
 
-    print(mogelijkheden)
+    print(lijst, len(lijst))
 
 gen()
 
